45ac2226d917ec9c2aad51fa4e3f41255f24e7c5
Jarren San Jose <sanjose.jarren@gmail.com>
2025-01-23 21:18:46 -0800

    add and display logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <Image 
+          src="/logo.png"
+          alt="Help LA Heal Logo"
+          width={120}
+          height={120}
+          priority
+        />
         <h1 className="text-4xl font-bold">
           Help LA Heal
         </h1>
